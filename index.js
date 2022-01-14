46c8af9b8cd3f4f8ce83c759d11a3bc7bfd86bea
Anton <trof3039@gmail.com>
2022-01-14 16:18:06 +0200

    first commit after configuration

---
+++ index.js
@@ -0,0 +1,7 @@
+var a = 12;
+var b = 14;
+b = 134;
+var c = 223;
+var arr = [];
+arr.push(c);
+console.log(a, '+', b, '=', a + b);
---
+++ index.ts
@@ -0,0 +1,11 @@
+const a: number = 12;
+
+let b: number = 14;
+b = 134;
+
+const c = 223;
+
+const arr = [];
+arr.push(c);
+
+console.log(a, '+', b, '=', a + b);
